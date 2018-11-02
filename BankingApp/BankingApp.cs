c5a128b6ca4782e018ff574a5f5191bffc8a6d20
Kyle <kyle.evitts@gmail.com>
2018-11-01 23:08:07 -0700

    Initial pass at builder syntax for command line argument parsing.

---
+++ BankingApp/BankingApp.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using BankingApp.Models;
+using McMaster.Extensions.CommandLineUtils;
+
+namespace BankingApp
+{
+    class BankingApp
+    {
+        private ICollection<User> Users = new List<User>();
+        private User LoggedInUser;
+        private bool IsAuthenticated {
+            get
+            {
+                return LoggedInUser != null;
+            }
+        }
+
+        private static void ConfigureApp(CommandLineApplication app)
+        {
+            app.Name = "BankingApp";
+            app.Description = "A simple banking app for recording transactions.";
+            app.HelpOption(true);
+            app.Command("login", loginCmd =>
+            {
+                loginCmd.Description = "The command to log-in to the app.";
+                loginCmd.OnExecute(() =>
+                {
+                    Console.WriteLine(loginCmd.Name);
+                });
+            });
+            app.Command("exit", loginCmd =>
+            {
+                loginCmd.Description = "The command to exit the app.";
+                loginCmd.OnExecute(() =>
+                {
+                    Console.WriteLine(loginCmd.Name);
+                });
+            });
+        }
+
+        static void Main(string[] args)
+        {
+            CommandLineApplication app = new CommandLineApplication();
+            ConfigureApp(app);
+            app.Execute(args);
+            args = Prompt.GetString("bankingapp> ").Split(' ');
+            app.Execute(args);
+            args = Prompt.GetString("bankingapp> ").Split(' ');
+            app.Execute(args);
+            args = Prompt.GetString("bankingapp> ").Split(' ');
+            app.Execute(args);
+            Console.ReadKey();
+        }
+    }
+}
